b680fd91c5e48f784a3f5250153aef88f59b8a8f
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2025-12-06 11:00:50 +0530

    Fix formatting of GoogleOAuthProvider clientId

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId = 1032992808612-anjnvceqmf5ma4tou9n0jme21tpginds.apps.googleusercontent.com
+  <GoogleOAuthProvider clientId = "1032992808612-anjnvceqmf5ma4tou9n0jme21tpginds.apps.googleusercontent.com"
     <BrowserRouter>
       <App />
     </BrowserRouter>
