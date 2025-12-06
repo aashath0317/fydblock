457d9ef5db2b69f4d414e503910490f122596ce8
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2025-12-06 10:59:36 +0530

    Update GoogleOAuthProvider clientId in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // <--- IMPORT THIS
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
+  <GoogleOAuthProvider clientId = 1032992808612-anjnvceqmf5ma4tou9n0jme21tpginds.apps.googleusercontent.com
     <BrowserRouter>
       <App />
     </BrowserRouter>
