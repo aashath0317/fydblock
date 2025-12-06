4500a8bb95001f988f14a6bc489bf7f5851318e5
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2025-12-06 11:03:06 +0530

    Refactor GoogleOAuthProvider usage in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App.jsx'
 import './index.css'
-import { GoogleOAuthProvider } from '@react-oauth/google';
+
+// 🔴 REPLACE THIS STRING with the ID you copied from Google Cloud Console
+const GOOGLE_CLIENT_ID = "1032992808612-anjnvceqmf5ma4tou9n0jme21tpginds.apps.googleusercontent.com"; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId = "1032992808612-anjnvceqmf5ma4tou9n0jme21tpginds.apps.googleusercontent.com"
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </GoogleOAuthProvider>
-);
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GoogleOAuthProvider>
+  </React.StrictMode>,
+)
