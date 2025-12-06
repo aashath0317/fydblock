75dcfb323d97c3d3698603dce7b65e9f07179815
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2025-12-06 10:55:01 +0530

    Wrap App component with GoogleOAuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom' // <--- IMPORT THIS
 import App from './App.jsx'
 import './index.css'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/* Wrap App inside BrowserRouter */}
+  <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </GoogleOAuthProvider>
+);
